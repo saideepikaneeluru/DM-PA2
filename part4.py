649645f1ca5268a945a6f6d19e1b4a9cb8a3fd79
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-03-31 19:52:46 -0400

    Update part4.py

--- part4.py
+++ part4.py
@@ -110,7 +110,7 @@ def compute():
     """
 
     # dct is the function described above in 4.C
-    dct = answers["4A: modified function"] = fit_modified
+    dct = answers["4C: modified function"] = fit_modified
 
     return answers
 
